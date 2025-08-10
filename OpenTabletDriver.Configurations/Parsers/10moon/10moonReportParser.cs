9fcee7f82b52285cd9ad6c23fac61e7b964c43b9
NelsonGomesNeto <ngomesneto@outlook.com>
2025-08-10 00:28:54 -0300

    Try to fix 10moon parser

--- OpenTabletDriver.Configurations/Parsers/10moon/10moonReportParser.cs
+++ OpenTabletDriver.Configurations/Parsers/10moon/10moonReportParser.cs
@@ -7,10 +7,12 @@ namespace OpenTabletDriver.Configurations.Parsers.TenMoon
     {
         public IDeviceReport Parse(byte[] report)
         {
-            if (report[11] != 0xFF)
-                return new TenMoonAuxReport(report);
-            else
-                return new TenMoonTabletReport(report);
+            // // This is buggy since blocks using the tablet when any of the
+            // // aux buttons are used hahaha
+            // if (report[11] != 0xFF)
+            //     return new TenMoonAuxReport(report);
+            // else
+            return new TenMoonTabletReport(report);
         }
     }
 }
--- OpenTabletDriver.Configurations/Parsers/10moon/10moonTabletReport.cs
+++ OpenTabletDriver.Configurations/Parsers/10moon/10moonTabletReport.cs
@@ -7,30 +7,82 @@ using OpenTabletDriver.Plugin.Tablet;
 
 namespace OpenTabletDriver.Configurations.Parsers.TenMoon
 {
-    public struct TenMoonTabletReport : ITabletReport
+    public struct TenMoonTabletReport : ITabletReport, IAuxReport
     {
         public TenMoonTabletReport(byte[] report)
         {
             Raw = report;
 
-            Position = new Vector2
-            {
-                X = report[1] << 8 | report[2],
-                Y = Math.Max((short)(report[3] << 8 | report[4]), (short)0)
-            };
+            // New way
+            var x = report[1] << 8 | report[2];
+            var y = report[3] << 8 | report[4];
+            // don't ask me why it's like this
+            if ((y & 0x8000) != 0)
+                y = (ushort)(0x8F - (ushort)(y & 0x7FFF));
+            else
+                y += 0x8F;
+            Position = new Vector2(x, y);
+            // Old way
+            // Position = new Vector2
+            // {
+            //     X = report[1] << 8 | report[2],
+            //     Y = Math.Max((short)(report[3] << 8 | report[4]), (short)0)
+            // };
 
-            var buttonPressed = (report[9] & 6) != 0;
-            var prePressure = report[5] << 8 | report[6];
-            Pressure = (uint)(0x0672 - (prePressure - (buttonPressed ? 50 : 0)));
+            // New way
+            ushort prePressure = (ushort)(report[5] << 8 | report[6]);
+            // ushort calibratedMax = (ushort)(report[7] << 8 | report[8]);
+            // I found this manually looking at the minimum values at
+            // "prePressure".
+            ushort calibratedMax = (ushort)(0x05 << 8 | 0xCE);
+            ushort pressure = (ushort)(calibratedMax - prePressure);
+            if ((pressure & 0x8000) != 0 || report[10] == 0x0F)
+                pressure = 0;
+            Pressure = pressure;
+
+            // Old way
+            // var buttonPressed = (report[9] & 6) != 0;
+            // var prePressure = report[5] << 8 | report[6];
+            // // Pressure = (uint)(0x0672 - (prePressure - (buttonPressed ? 50 : 0)));
+            // Pressure = (uint)(1500 - (prePressure - (buttonPressed ? 50 : 0)));
 
             PenButtons = new bool[]
             {
-                report[9].IsBitSet(2),
-                (report[9] & 6) == 6
+                // new?
+                (report[9] & 0b110) == 0b100,
+                (report[9] & 0b110) == 0b110
+                // working
+                // report[9] == 4,
+                // report[9] == 6
+                // old
+                // report[9].IsBitSet(2),
+                // (report[9] & 6) == 6
+            };
+
+            AuxButtons = new bool[]
+            {
+                !report[12].IsBitSet(0), // Eighth
+                !report[12].IsBitSet(5), // Nineth
+
+                !report[12].IsBitSet(1), // First
+                !report[12].IsBitSet(4), // Seventh
+
+                !report[11].IsBitSet(6), // Third
+                !report[11].IsBitSet(7), // Second
+
+                !report[11].IsBitSet(5), // Fourth
+                !report[11].IsBitSet(4), // Fifth
+
+                // These buttons don't exist on my tablet hahaha
+                !report[11].IsBitSet(3), // Sixth
+                !report[11].IsBitSet(0), // Tenth
+                !report[11].IsBitSet(1), // Eleventh
+                !report[11].IsBitSet(2) // Twelveth
             };
         }
 
         public byte[] Raw { set; get; }
+        public bool[] AuxButtons { set; get; }
         public Vector2 Position { set; get; }
         public uint Pressure { set; get; }
         public bool[] PenButtons { set; get; }
